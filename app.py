28e1b0a54c90e14951421f450806d47f8fc3442c
primitt <prianshmittra@gmail.com>
2021-10-31 14:58:35 -0700

    .

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask.templating import render_template
-app = Flask(__name__, template_folder='/html')
+app = Flask(__name__, template_folder='html/')
 
 @app.route("/")
 def home():
