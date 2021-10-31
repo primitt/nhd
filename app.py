2a614186d9b10715738ba45669a6d82e573b9532
primboi <prianshmittra@gmail.com>
2021-10-31 16:20:50 -0700

    Pushing this cause git told me to
    git is weird

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask
 from flask.templating import render_template
 app = Flask(__name__, template_folder='html/')
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("home.html")
-
-
+@app.route("/")
+def start():
+    return render_template("hyper.html")
+@app.route("/indepth")
+def indepth():
+    return render_template("indepth.html")
+@app.route("/devcredits")
+def indepth():
+    return render_template("devcredits.html")
+@app.errorhandler(404)
+def error404():
+    return render_template("404.html")
 
